a650595a512167d35d0d48ba52eb20cb5549e69b
shuvamk <kshuvam564@gmail.com>
2020-09-06 21:36:54 -0700

    First Commit

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+function Login() {
+    return (
+        <div className='login'>
+            <img src="https://i.pinimg.com/originals/27/6f/27/276f273d11f8b9dbc0a9c55bb38ea8c6.gif" alt=''></img>
+            <a >LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
